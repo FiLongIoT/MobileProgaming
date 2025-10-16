7c61c05f152bc62e83b2a949988e4619c36dff7e
FiLongIoT <filong005@gmail.com>
2025-10-16 11:36:46 +0700

    bai tap Crud database firebase

---
+++ crudfirebasesCloudStoreAndroid/app/src/main/java/com/example/crudfirebasescloudstoreandroid/Course.kt
@@ -0,0 +1,8 @@
+package com.example.crudfirebasescloudstoreandroid
+
+data class Course(
+// on below line creating variables.
+var courseName: String? = "",
+var courseDuration: String? = "",
+var courseDescription: String? = ""
+)
